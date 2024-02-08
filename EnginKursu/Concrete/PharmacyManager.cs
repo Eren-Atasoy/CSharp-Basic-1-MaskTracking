fa6bf0960d1b586733690d31af0363bb5741d77c
erena <erena@ERMI>
2024-02-08 12:52:31 +0300

    Add project files.

---
+++ EnginKursu/Concrete/PharmacyManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PharmacyManager : ISuppliersService
+    {
+        private IApplicantService _applicantService;
+
+        public PharmacyManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            if(_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine($"Eczaneden türk vatandaşı {person.FirstName} adına Maske Verilemedi");
+            }
+            else
+            {
+                Console.WriteLine($"Eczaneden yabancı uyruklu {person.FirstName} adına Maske VERİLDİ");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,39 @@
+﻿ using Entities.Concrete;
+using Business.Concrete;
+using System;
+
+
+namespace Workaround
+{
+    public class Program
+    {
+        static void Main()
+
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Eren";
+            person1.LastName = "Atasoy";
+            person1.NatitonalIdenty = 1453;
+            person1.DateOfBirthYear = 2020;
+
+            Person person2 = new Person();
+            person2.FirstName = "Necati";
+            person2.LastName = "Şaşmaz";
+            person2.NatitonalIdenty = 1452;
+            person2.DateOfBirthYear = 2010;
+
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+            PharmacyManager pharmacyManager = new PharmacyManager(new ForeignerManager());
+            pharmacyManager.GiveMask(person2);
+        }
+
+        
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+}
